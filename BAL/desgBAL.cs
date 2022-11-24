5087e8886b23668c5e00f3af5a4902105a83b45a
Renish S <Renish@NTP-LAP-835.chn.nestgroup.net>
2022-11-24 08:28:25 +0530

    first commit\

---
+++ BAL/desgBAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace assig6.BAL
+{
+    public class desgBAL
+    {
+    }
+}
---
+++ DAL/desgDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace assig6.DAL
+{
+    public class desgDAL
+    {
+    }
+}
